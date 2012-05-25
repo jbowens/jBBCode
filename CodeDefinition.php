2685f252cfaf999441109d51670472fda88c0224
Jackson Owens <jackson_owens@brown.edu>
2012-05-25 12:43:48 -0400

    first commit (v.1.0.3)

---
+++ CodeDefinition.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace JBBCode;
+
+/**
+ * @author Jackson Owens
+ * 
+ * This class represents a BBCode Definition.
+ */
+class CodeDefinition {
+
+	/* NOTE: THIS PROPERTY SHOULD ALWAYS BE LOWERCASE; USE setTagName() TO ENSURE THIS */
+	protected $tagName; 
+
+	/* Whether or not this CodeDefinition uses an option parameter. */
+	protected $useOption;
+
+	/* The replacement text to be used for simple CodeDefinitions */
+	protected $replacementText;
+
+	/* Whether or not to parse elements of this definition's contents */
+	protected $parseContent;
+
+	/* How many of this element type may be nested within each other */
+	protected $nestLimit;
+
+	/* How many of this element type have been seen */
+	protected $elCounter;
+	
+	/**
+	 * Constructs a new CodeDefinition.
+	 * 
+	 * You WILL want to override this if you extend this class and your new definition accepts an option, doesn't parse
+	 * its content, etc.
+	 */
+	public function __construct()
+	{
+		$this->useOption = false;
+		$this->parseContent = true;
+		$this->nestLimit = -1;
+		$this->elCounter = 0;
+	}
+	
+	/**
+	 * Accepts an ElementNode that is defined by this CodeDefinition and returns the HTML markup of the element.
+	 * This is a commonly overridden class for custom CodeDefinitions so that the content can be directly manipulated.
+	 * 
+	 * @param el the element to return an html representation of
+	 * 
+	 * @return the parsed html of this element (INCLUDING ITS CHILDREN)
+	 */
+	public function asHtml( ElementNode $el )
+	{
+		$html = $this->getReplacementText();
+		
+		if( $this->usesOption() )
+		{
+			$html = str_ireplace('{option}', $el->getAttribute(), $html);
+		}
+		
+		if( $this->parseContent() )
+		{
+			$content = "";
+			foreach( $el->getChildren() as $child )
+				$content .= $child->getAsHTML();	
+		}
+		else
+		{
+			$content = "";
+			foreach( $el->getChildren() as $child )
+				$content .= $child->getAsBBCode();
+		}
+		
+		$html = str_ireplace('{param}', $content, $html);
+		
+		return $html;
+		
+	}
+	
+	/**
+	 * Returns the tag name of this code definition
+	 * 
+	 * @return this definition's associated tag name
+	 */
+	public function getTagName()
+	{
+		return $this->tagName;
+	}
+	
+	/**
+	 * Returns the replacement text of this code definition. This usually has little, if any meaning if the
+	 * CodeDefinition class was extended. For default, html replacement CodeDefinitions this returns the html
+	 * markup for the definition.
+	 * 
+	 * @return the replacement text of this CodeDefinition
+	 */
+	public function getReplacementText()
+	{
+		return $this->replacementText;
+	}
+	
+	/**
+	 * Returns whether or not this CodeDefinition uses the optional {option}
+	 * 
+	 * @return true if this CodeDefinition uses the option, false otherwise
+	 */
+	public function usesOption()
+	{
+		return $this->useOption;
+	}
+	
+	/**
+	 * Returns whether or not this CodeDefnition parses elements contained within it, or just treats its children as text.
+	 * 
+	 * @return true if this CodeDefinition parses elements contained within itself
+	 */
+	public function parseContent()
+	{
+		return $this->parseContent;
+	}
+	
+	/**
+	 * 
+	 * NOT YET SUPPORTED
+	 * 
+	 */
+	public function getNestLimit()
+	{
+		return $this->nestLimit;
+	}
+	
+	/**
+	 * Sets the tag name of this CodeDefinition
+	 * 
+	 * @param the new tag name of this definition
+	 */
+	public function setTagName( $tagName )
+	{
+		$this->tagName = strtolower($tagName);
+	}
+	
+	/**
+	 * Sets the html replacement text of this CodeDefinition
+	 * 
+	 * @param the new replacement text
+	 */
+	public function setReplacementText( $txt )
+	{
+		$this->replacementText = $txt;
+	}
+	
+	/**
+	 * Sets whether or not this CodeDefinition uses the {option}
+	 * 
+	 * @param boolean $bool
+	 */
+	public function setUseOption( $bool )
+	{
+		$this->useOption = $bool;
+	}
+
+	/**
+	 * 
+	 * Sets whether or not this CodeDefinition allows its children to be parsed as html
+	 * 
+	 * @param boolean $bool
+	 * 
+	 */
+	public function setParseContent( $bool )
+	{
+		$this->parseContent = $bool;
+	}
+	
+	/**
+	 * 
+	 * NOT YET SUPPORTED
+	 * 
+	 * @param integer $limit
+	 * 
+	 */
+	public function setNestLimit( $limit = -1 )
+	{
+		$this->nestLimit = $limit;
+	}
+	
+	/**
+	 * 
+	 * Increments the element counter. This is used for tracking depth of elements of the same type for next limits.
+	 * 
+	 * @return void
+	 * 
+	 */
+	public function incrementCounter()
+	{
+		$this->elCounter++;
+	}
+	/**
+	 * 
+	 * Decrements the element counter.
+	 * 
+	 * @return void
+	 * 
+	 */
+	public function decrementCounter()
+	{
+		$this->elCounter--;
+	}
+
+	/**
+	 * 
+	 * Resets the element counter.
+	 * 
+	 */
+	public function resetCounter()
+	{
+		$this->elCounter = 0;
+	}
+	
+	/**
+	 * 
+	 * Returns the current value of the element counter.
+	 * 
+	 * @return int
+	 * 
+	 */
+	public function getCounter()
+	{
+		return $this->elCounter;
+	}
+}
+
+?>
---
+++ TokenManager.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace JBBCode;
+
+/**
+ * @author Jackson Owens
+ * 
+ * The TokenManager is used when constructing the parse tree. Before parsing begins, the TokenManager separates the string into
+ * left and right brackets ("[", "]") and string to make parsing easier.
+ * 
+ */
+class TokenManager {
+	
+	protected $tokens;
+	protected $i = 0;
+	
+	/**
+	 * Tokenizes the inputted string
+	 * 
+	 * @param string $str	the string to tokenize
+	 */
+	public function __construct( $str ) {
+		$this->tokens = array();
+		foreach(preg_split('/([\[\]])/', $str, -1, PREG_SPLIT_DELIM_CAPTURE) as $s)
+		{
+			if( $s != "" )
+				array_push($this->tokens, $s);
+		}
+		$this->restart();
+	}
+	
+	/**
+	 * Returns true if there is another token after the current one.
+	 * 
+	 * @return true if there is another token to be read
+	 */
+	public function hasNext() {
+		return count( $this->tokens ) > ($this->i + 1);
+	}
+	
+	/**
+	 * Returns true if there is a current token.
+	 *
+	 * @return true if there is a current token
+	 */
+	public function hasCurrent() {
+		return count( $this->tokens ) > $this->i;
+	}
+
+	/**
+	 * Returns the current token.
+	 * 
+	 * @return the current token
+	 */
+	public function current() {
+		return $this->tokens[$this->i];
+	}	
+	
+	/**
+	 * Alias for getCurrent()
+	 *
+	 * @return the current token
+	 */
+	public function getCurrent() {
+		return $this->current();
+	}
+	
+	/**
+	 * Returns the next token.
+	 * 
+	 * @return the next token
+	 */
+	public function next() {
+		if( $this->hasNext() )
+			return $this->tokens[$this->i+1];
+		else
+			return null;
+	}
+	
+	/**
+	 * Alias for next().
+	 *
+	 * @return the next token
+	 */
+	public function getNext() {
+		return $this->next();
+	}
+	
+	/**
+	 * Returns the array of all the tokens.
+	 * 
+	 * @return all tokens
+	 */
+	public function getAllTokens() {
+		return $this->tokens;
+	}
+	
+	/**
+	 * Moves the pointer back to the first token.
+	 */
+	public function restart() {
+		$this->i = 0;
+	}
+	
+	/**
+	 * Advances the token pointer to the next token.
+	 */
+	public function advance() {
+		$this->i++;
+	}
+	
+}
+
